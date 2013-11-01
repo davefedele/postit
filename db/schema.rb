bcf276ce18f829ca8b36dc4e145103ec45df0f80
David Fedele <dave.fedele@gmail.com>
2013-10-31 23:15:16 -0700

    added User model and users table created associations 1:M between user and posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131101053816) do
+ActiveRecord::Schema.define(version: 20131101055432) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.string   "url"
     t.text     "description"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
