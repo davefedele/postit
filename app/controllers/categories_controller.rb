87c18212d77fa8745937ab89c04ba7d7db502a5a
David Fedele <dave.fedele@gmail.com>
2013-11-10 12:56:32 -0800

    removed edit capability from category updated category controller cleaned up code with partials

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,49 +1,28 @@
 class CategoriesController < ApplicationController
-before_action :set_post, only: [:show,:edit,:update]
-
-  def index
-    @category = Category.all
-  end
-
-  def show
-  end
 
   def new
     @category = Category.new
   end
 
   def create
-    @category = Category.new(post_params)
+    @category = Category.new(category_params)
 
     if @category.save
-      flash[:notice] = "The category was created."
-      redirect_to posts_path
+       flash[:notice] = "The category was created."
+      redirect_to root_path
     else
       render :new
     end
-
-  end
-
-  def edit
-  end
-
-  def update
-    if @category.update(post_params)
-      flash[:notice] = "The category was updated."
-      redirect_to category_path(@category)
-    else
-      render :edit
-    end
   end
   
+  def show
+    @category = Category.find(params[:id])
+  end
+
   private
 
-  def post_params
-    params.require(:category).permit!
-  end
-
-  def set_post
-    @category = Category.find(params[:id ])    
-  end
+    def category_params
+      params.require(:category).permit(:name)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
   resources :posts, except: [:destroy] do
-    resources :comments, only: [:create]
+    resources :comments, only: [:new, :create, :show]
    end
   resources :categories, except: [:index, :destroy]
 end
