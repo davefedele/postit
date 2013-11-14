21726d7407f93d53e2d095f86954bf7e74ffa09b
David Fedele <dave.fedele@gmail.com>
2013-11-13 23:06:55 -0800

    added users controller and user model created form partial and edit and new routes added routes for session

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = "You are registered."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Updated your profile"
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
+end
