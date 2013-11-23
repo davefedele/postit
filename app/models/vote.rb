bb97ee0b2d1806a22601d54dc5af8bc980b40761
David Fedele <dave.fedele@gmail.com>
2013-11-22 22:58:50 -0800

    added missing relationship to votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,7 @@
 class Vote < ActiveRecord::Base
   belongs_to  :creator, class_name: 'User', foreign_key: 'user_id'
   belongs_to  :voteable, polymorphic: true
-
+  has_many    :votes, as: :voteable
+  
   validates_uniqueness_of :creator, scope: :voteable
 end
