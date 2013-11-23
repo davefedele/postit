7c287f2d98377d24061722909dc550fc89b6b393
David Fedele <dave.fedele@gmail.com>
2013-11-23 14:18:33 -0800

    tweaked post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   has_many    :comments
   has_many    :post_categories 
   has_many    :categories, through: :post_categories 
-  has_many    :votes, as: :polymorphic
+  has_many    :votes, as: :voteable
 
   before_save :generate_slug
 
